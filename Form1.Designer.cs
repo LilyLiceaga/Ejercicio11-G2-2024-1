8baed85a4935cd5da89c6dc32a69b30dc65f887e
LilyLiceaga <lily.liceaga@gmail.com>
2023-10-06 11:34:58 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Ejercicio10_G2_2024_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbTelefono = new System.Windows.Forms.Label();
+            this.lbEdad = new System.Windows.Forms.Label();
+            this.txtbNombre = new System.Windows.Forms.TextBox();
+            this.txtbTelefono = new System.Windows.Forms.TextBox();
+            this.txtbEdad = new System.Windows.Forms.TextBox();
+            this.lbGuardado = new System.Windows.Forms.Label();
+            this.lbDireccion = new System.Windows.Forms.Label();
+            this.txtbDireccion = new System.Windows.Forms.TextBox();
+            this.btnGuardar2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSiguiente.Location = new System.Drawing.Point(355, 408);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(146, 50);
+            this.btnSiguiente.TabIndex = 0;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombre.Location = new System.Drawing.Point(86, 106);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(78, 22);
+            this.lbNombre.TabIndex = 1;
+            this.lbNombre.Text = "Nombre:";
+            // 
+            // lbTelefono
+            // 
+            this.lbTelefono.AutoSize = true;
+            this.lbTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTelefono.Location = new System.Drawing.Point(86, 177);
+            this.lbTelefono.Name = "lbTelefono";
+            this.lbTelefono.Size = new System.Drawing.Size(86, 22);
+            this.lbTelefono.TabIndex = 1;
+            this.lbTelefono.Text = "Teléfono:";
+            // 
+            // lbEdad
+            // 
+            this.lbEdad.AutoSize = true;
+            this.lbEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEdad.Location = new System.Drawing.Point(86, 257);
+            this.lbEdad.Name = "lbEdad";
+            this.lbEdad.Size = new System.Drawing.Size(57, 22);
+            this.lbEdad.TabIndex = 1;
+            this.lbEdad.Text = "Edad:";
+            // 
+            // txtbNombre
+            // 
+            this.txtbNombre.Location = new System.Drawing.Point(185, 105);
+            this.txtbNombre.Name = "txtbNombre";
+            this.txtbNombre.Size = new System.Drawing.Size(364, 22);
+            this.txtbNombre.TabIndex = 2;
+            // 
+            // txtbTelefono
+            // 
+            this.txtbTelefono.Location = new System.Drawing.Point(185, 177);
+            this.txtbTelefono.Name = "txtbTelefono";
+            this.txtbTelefono.Size = new System.Drawing.Size(364, 22);
+            this.txtbTelefono.TabIndex = 2;
+            // 
+            // txtbEdad
+            // 
+            this.txtbEdad.Location = new System.Drawing.Point(185, 257);
+            this.txtbEdad.Name = "txtbEdad";
+            this.txtbEdad.Size = new System.Drawing.Size(364, 22);
+            this.txtbEdad.TabIndex = 2;
+            // 
+            // lbGuardado
+            // 
+            this.lbGuardado.AutoSize = true;
+            this.lbGuardado.Location = new System.Drawing.Point(90, 46);
+            this.lbGuardado.Name = "lbGuardado";
+            this.lbGuardado.Size = new System.Drawing.Size(40, 16);
+            this.lbGuardado.TabIndex = 3;
+            this.lbGuardado.Text = "           ";
+            // 
+            // lbDireccion
+            // 
+            this.lbDireccion.AutoSize = true;
+            this.lbDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDireccion.Location = new System.Drawing.Point(86, 332);
+            this.lbDireccion.Name = "lbDireccion";
+            this.lbDireccion.Size = new System.Drawing.Size(90, 22);
+            this.lbDireccion.TabIndex = 1;
+            this.lbDireccion.Text = "Dirección:";
+            // 
+            // txtbDireccion
+            // 
+            this.txtbDireccion.Location = new System.Drawing.Point(185, 332);
+            this.txtbDireccion.Name = "txtbDireccion";
+            this.txtbDireccion.Size = new System.Drawing.Size(364, 22);
+            this.txtbDireccion.TabIndex = 2;
+            // 
+            // btnGuardar2
+            // 
+            this.btnGuardar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar2.Location = new System.Drawing.Point(128, 408);
+            this.btnGuardar2.Name = "btnGuardar2";
+            this.btnGuardar2.Size = new System.Drawing.Size(146, 50);
+            this.btnGuardar2.TabIndex = 0;
+            this.btnGuardar2.Text = "Guardar";
+            this.btnGuardar2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 554);
+            this.Controls.Add(this.lbGuardado);
+            this.Controls.Add(this.txtbEdad);
+            this.Controls.Add(this.txtbDireccion);
+            this.Controls.Add(this.txtbTelefono);
+            this.Controls.Add(this.txtbNombre);
+            this.Controls.Add(this.lbEdad);
+            this.Controls.Add(this.lbDireccion);
+            this.Controls.Add(this.lbTelefono);
+            this.Controls.Add(this.lbNombre);
+            this.Controls.Add(this.btnGuardar2);
+            this.Controls.Add(this.btnSiguiente);
+            this.Name = "Form1";
+            this.Text = "Agenda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbTelefono;
+        private System.Windows.Forms.Label lbEdad;
+        private System.Windows.Forms.TextBox txtbNombre;
+        private System.Windows.Forms.TextBox txtbTelefono;
+        private System.Windows.Forms.TextBox txtbEdad;
+        private System.Windows.Forms.Label lbGuardado;
+        private System.Windows.Forms.Label lbDireccion;
+        private System.Windows.Forms.TextBox txtbDireccion;
+        private System.Windows.Forms.Button btnGuardar2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Ejercicio10_G2_2024_1
+{
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
